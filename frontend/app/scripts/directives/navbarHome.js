7fd28bfad790ec9011d368610d67b8505a9239e1
Nico-Magnoni <niko_independiente@live.com.ar>
2016-06-02 21:06:26 -0300

    directiva navbar home creada

---
+++ frontend/app/scripts/directives/navbarHome.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nico on 02/06/16.
+ */
+angular.module('dutymap')
+  .directive('navbarHome', function () {
+    return {
+
+      templateUrl:'views/directives/navbar-home.html',
+      restrict: 'E',
+      replace:true,
+    };
+  });

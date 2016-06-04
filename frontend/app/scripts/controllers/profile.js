ba22a2745a940f24893afaeee16398bf76d511b8
JonaVillarroel <jvillarroel.dev@gmail.com>
2016-06-04 11:03:49 -0300

    WIP profile stars

--- frontend/app/scripts/controllers/profile.js
+++ frontend/app/scripts/controllers/profile.js
@@ -17,7 +17,9 @@ angular.module('dutymap')
           $scope.works = profileSelected.works;
           $scope.mainWork = $scope.works[0];
 
-
+          $scope.starRating = 4; //TODO Hardcodeado agregar a rating y sacar de la variable
+          $scope.stars = _.range($scope.starRating);
+          $scope.emptyStars = _.range(5 - $scope.starRating);
 
           console.log($scope.profile);
 

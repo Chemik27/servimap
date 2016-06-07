74101ec203c31ec585dd72cc70ef91206d8ff4ab
Nico-Magnoni <niko_independiente@live.com.ar>
2016-06-07 01:02:43 -0300

    register arreglado

--- frontend/app/scripts/controllers/register.js
+++ frontend/app/scripts/controllers/register.js
@@ -1,7 +1,7 @@
 /**
  * Created by nico on 27/05/16.
  */
-angular.module('dutymap',[])
+angular.module('dutymap')
   .controller('RegisterCtrl', ['$scope', '$http' , function ($scope, $http) {
 
     $scope.userType = function(type) {

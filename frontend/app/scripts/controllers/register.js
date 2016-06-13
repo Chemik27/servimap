8d7d4c579c936bb42140991931b439866ef76b9e
Nico-Magnoni <niko_independiente@live.com.ar>
2016-06-13 17:00:59 -0300

    validaciones proveedor

--- frontend/app/scripts/controllers/register.js
+++ frontend/app/scripts/controllers/register.js
@@ -7,13 +7,12 @@ angular.module('dutymap')
     $scope.userType = function(type) {
 
       $scope.filter = type;
-
     };
 
     $scope.onlyNumbers= /^\d+$/;
+    $scope.document=/^(\d{8})$/;
     $scope.onlyChar=/^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/
     //^[A-Za-z']+( [A-Za-z']+)*$/;
-
 
     // al menos una letra, un número y 8 caracteres
     $scope.passwordRegex=/^(?=.*[a-z])(?=.*\d)[a-zA-Z\d]{8,}$/;
@@ -22,16 +21,26 @@ angular.module('dutymap')
     // (?=.*[A-Z])
 
     $scope.user={};
+    $scope.provider={};
 
-    $scope.userForm=function(user){
-      
-      console.log(user);
+    $scope.compare=function(){
+      if($scope.provider.password != $scope.provider.passwordConfirmation) {
+        $scope.noMatchea = true;
+        $scope.providerForm.passwordConfirmation.$setValidity('noMatchea', false);
 
+      }
+      else{
+        $scope.noMatchea = false;
+        $scope.providerForm.passwordConfirmation.$setValidity('noMatchea', true);
+      }
     };
 
-    $scope.providerRegister =function(){
-      alert("proveedor")
-    }
+    $scope.registerUser=function(user){
+      console.log(user);
+    };
 
+    $scope.registerProvider =function(provider){
+      console.log(provider);
+    }
     }]);
 

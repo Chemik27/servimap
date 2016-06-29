1b67feb496fdb4aeb3d2d158b0911ddde2596d3e
Jona <jonathan.villarroel@redb.ee>
2016-06-29 09:24:39 -0300

    Fixes y se agregan request excluidos de seguridad

--- backend/src/main/java/com/tpfinal/security/WebSecurityConfig.java
+++ backend/src/main/java/com/tpfinal/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -43,11 +44,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests()
-                .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
-                .authenticated().and().csrf()
-                .csrfTokenRepository(csrfTokenRepository()).and()
-                .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+        http.httpBasic()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/register/*").permitAll()
+                    .antMatchers("/user").permitAll()
+                    .antMatchers("/profile/*").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf()
+                    .csrfTokenRepository(csrfTokenRepository()).and()
+                    .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
     }
 
     private Filter csrfHeaderFilter() {

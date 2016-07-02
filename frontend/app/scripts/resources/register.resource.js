f53a669bfed7c5f02c46a049f1a68feff3f6dcbf
Maximilianno <s.maximilianno@gmail.com>
2016-07-02 18:15:00 -0300

    Borrado de bower_components duplicado

--- frontend/app/scripts/resources/register.resource.js
+++ frontend/app/scripts/resources/register.resource.js
@@ -4,10 +4,24 @@
 'use strict';
 
 angular.module('dutymap')
-  .factory('RegisterResources', ['$resource',function RegisterResources($resource) {
-    return $resource('api/register/dates', {},{
-      // method: 'GET',
-      // url: 'api/work/worksByCategory/:id',
-      // isArray : true
-    });
+  .factory('RegisterResources', ['$resource',
+    function RegisterResources($resource) {
+      var method1Logic = function() {
+        return $resource('api/register/dates', {});
+      };
+
+
+      return {
+        lala: method1Logic
+      };
   }]);
+
+// angular.module('dutymap')
+//   .factory('RegisterResources', ['$resource',
+//     function RegisterResources($resource) {
+//       return $resource('api/register/dates', {},{
+//         // method: 'GET',
+//         // url: 'api/work/worksByCa xtegory/:id',
+//         // isArray : true
+//       });
+//     }]);

38fdd70c6b68937f40d83ab06c262aa2d1f1991d
Nico-Magnoni <niko_independiente@live.com.ar>
2016-07-02 01:28:30 -0300

    recurso modificado

--- frontend/app/scripts/resources/hire.resource.js
+++ frontend/app/scripts/resources/hire.resource.js
@@ -1,6 +1,19 @@
 angular.module('dutymap')
-  .factory('HireResources', ['$resource',function HireResources($resource) {
+  .factory('HireResources', ['$resource','$q',function HireResources($resource,$q) {
     return $resource('api/hire/:id', {id:'@id'},{
-     
+
+      createTransaction: function(transaction){
+        return $http.post('api/hire/:id/confirm',{id:'@id'})
+          .then(
+            function(response){
+              return response.data;
+            },
+            function(errResponse){
+              console.error('Error while creating transaction');
+              return $q.reject(errResponse);
+            }
+          );
+      },
+
     });
   }]);

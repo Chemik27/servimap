e34cf3b8d75bff484bb75e956f0865a7a1f873d1
Chemik27 <karii.gatica@gmail.com>
2016-07-03 13:47:46 -0300

    Cambios Karina

--- frontend/app/scripts/controllers/qualify.js
+++ frontend/app/scripts/controllers/qualify.js
@@ -8,8 +8,17 @@ angular.module('dutymap')
   .controller('QualifyUserCtrl', ['$scope',
     function ($scope ) {
 
-      $scope.rating = 5;
+      //Calificaciones 4
+      $scope.rating1 = 1;
+      $scope.rating2 = 1;
+      $scope.rating3 = 1;
+      $scope.rating4 = 1;
+      $scope.resultRating = 0;
       $scope.rateFunction = function(rating) {
-        alert('Rating selected - ' + rating);
-      }
+        //alert('Rating selected - ' + rating);
+      };
+      $scope.calcularRating= function(){
+        $scope.resultRating = Math.round(($scope.rating1 + $scope.rating2 + $scope.rating3 + $scope.rating4)/4);
+      };
+
     }]);

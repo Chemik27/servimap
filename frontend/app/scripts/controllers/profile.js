e4d1bde4427ca4bcc1cede459a0c35c811acd392
Jona <jonathan.villarroel@redb.ee>
2016-07-15 14:36:39 -0300

    WIP perfil

--- frontend/app/scripts/controllers/profile.js
+++ frontend/app/scripts/controllers/profile.js
@@ -12,12 +12,22 @@ angular.module('dutymap')
           function ($scope, $http, ProfileResources, profileSelected) {
             $scope.profile = profileSelected;
             $scope.user = profileSelected.user;
-            $scope.transactions = profileSelected.lastTransactions;
+            $scope.myTransactions = profileSelected.lastTransactions;
+            $scope.myServiceHistory = profileSelected.transactions;
             $scope.works = profileSelected.works;
             $scope.mainWork = $scope.works[0];
-            $scope.rating = profileSelected.rating[0];
+            if(profileSelected.rating != undefined){
+                $scope.rating = profileSelected.rating.finalRating;
+                $scope.comments = profileSelected.rating.comments;
+            }
+
+
+
+
+
 
             if($scope.rating != undefined){
+
               $scope.generalAverage= Math.round( ($scope.rating.reliability + $scope.rating.performance + $scope.rating.recommendation + $scope.rating.consideration)/4);
               $scope.starRating = $scope.generalAverage;
               $scope.stars = _.range($scope.starRating);
@@ -31,4 +41,5 @@ angular.module('dutymap')
               $scope.starRecommendation = _.range($scope.rating.recommendation);
               $scope.emptyStarsRecommendation = _.range(5 - $scope.rating.recommendation);
             }
+
     }]);

f9b6d59c0b5b4764346aa4a022ccc6c7c4ddacbc
Jona <jonathan.villarroel@redb.ee>
2016-07-15 21:14:41 -0300

    Marker by address

--- backend/src/main/java/com/tpfinal/domain/Address.java
+++ backend/src/main/java/com/tpfinal/domain/Address.java
@@ -11,19 +11,15 @@ public class Address {
     private Long idAddress;
     private String street;
     private Long number;
-    private Double latitude;
-    private Double length;
     private District district;
 
     public final static Long NO_WORK_ADDRESS = 1L;
     public final static Long SAME_RECIDENTIAL_ADDRESS = 2L;
     public final static Long NEW_ADDRESS = 3L;
 
-    public Address(String street, Long number, Double latitude, Double length, District district) {
+    public Address(String street, Long number, District district) {
         this.street = street;
         this.number = number;
-        this.latitude = latitude;
-        this.length = length;
         this.district = district;
     }
 
@@ -57,24 +53,6 @@ public class Address {
 
     public void setNumber(Long number) {
         this.number = number;
-    }
-
-    @Column(name = "latitude")
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    @Column(name = "length")
-    public Double getLength() {
-        return length;
-    }
-
-    public void setLength(Double length) {
-        this.length = length;
     }
 
     @ManyToOne(fetch = FetchType.EAGER)
